0d87f184a93516d6c413efb12a2599155b1b4fec
Felipe Vergara <felipevergalrostirola@gmail.com>
2026-09-04 19:56:40 -0300

    Add files via upload

---
+++ botao/botao/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace botao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(433, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SIM";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(183, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(421, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "QUER NAMORAR COMIGO?";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(229, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "NÃO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ botao/botao/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace botao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Claro que você aceita!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+        
+            Random rand = new Random();
+
+
+            int jumpX = rand.Next(20, 70);
+            int jumpY = rand.Next(20, 70);
+
+           
+            int newLeft = button2.Left;
+            int newTop = button2.Top;
+
+            if (rand.Next(0, 2) == 0)
+            {
+                newLeft = newLeft + jumpX; 
+            }
+            else
+            {
+                newLeft = newLeft - jumpX; 
+            }
+
+
+            if (rand.Next(0, 2) == 0)
+            {
+                newTop = newTop + jumpY; 
+            }
+            else
+            {
+                newTop = newTop - jumpY; 
+            }
+
+            int maxLeft = this.ClientSize.Width - button2.Width;
+            int maxTop = this.ClientSize.Height - button2.Height;
+
+            if (newLeft < 0) 
+                newLeft = 0;
+            if (newLeft > maxLeft)
+                newLeft = maxLeft;
+
+            if (newTop < 0) 
+                newTop = 0;
+
+            if (newTop > maxTop) 
+                newTop = maxTop;
+
+            button2.Location = new Point(newLeft, newTop);
+        }
+    }
+    
+}
